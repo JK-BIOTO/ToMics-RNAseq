db141a291c5204971e1c2f8756b1f043e9bd3e77
JK Choe <jk@bioto.co.kr>
2023-11-24 15:53:58 +0900

    test codes

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-dsjifdjsifjdi
+#!/usr/bin/env python3
+# _*_ encoding:utf-8 _*_
+
+import sys
+
+class nums:
+    def xx(i):
+        """makes double
+
+        Args:
+            i (int): integer
+        """
+        return i + i
+
+
+def test():
+    for i in range(10):
+        nums.xx(i)
+
+
+if __name__ == "__main__":
+    test()
